7ef4e5a82bc33fc8ba28e4450d1fa68719980606
Nickolas Lanasa <nick@nytekproductions.com>
2015-05-02 16:30:56 -0400

    Pods and project update commit

--- Pods/Headers/Public/RFMarkdownTextView/RFMarkdownToolbarButton.h
+++ Pods/Headers/Public/RFMarkdownTextView/RFMarkdownToolbarButton.h
@@ -1,1 +1,13 @@
-../../../RFMarkdownTextView/RFMarkdownTextView/RFMarkdownToolbarButton.h
+//
+//  RFMarkdownToolbarButton.h
+//  RFMarkdownTextViewDemo
+//
+//  Created by Rudd Fawcett on 12/19/13.
+//  Copyright (c) 2013 Rex Finn. All rights reserved.
+//
+
+#import "RFToolbarButton.h"
+
+@interface RFMarkdownToolbarButton : RFToolbarButton
+
+@end
