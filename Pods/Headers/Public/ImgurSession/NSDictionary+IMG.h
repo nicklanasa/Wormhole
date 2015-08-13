ec8378b4d5a6f282aeebcfab6e5e98305425e1d2
Nickolas Lanasa <nick@nytekproductions.com>
2015-08-13 12:58:29 -0400

    iPad storyboard relayout.

--- Pods/Headers/Public/ImgurSession/NSDictionary+IMG.h
+++ Pods/Headers/Public/ImgurSession/NSDictionary+IMG.h
@@ -1,1 +1,15 @@
-../../../ImgurSession/ImgurSession/NSDictionary+IMG.h
+//
+//  NSDictionary+IMG.h
+//  ImgurSession
+//
+//  Created by Geoff MacDonald on 2014-04-10.
+//  Copyright (c) 2014 GeoffMacDonald. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (IMG)
+
+-(NSDictionary *)IMG_cleanNull;
+
+@end
--- Pods/Headers/Public/NSDate+Calendar/NSDate+Year.h
+++ Pods/Headers/Public/NSDate+Calendar/NSDate+Year.h
@@ -1,1 +1,22 @@
-../../../NSDate+Calendar/Pod/NSDate+Year.h
+//
+//  NSDate+Year.h
+//  NSDate+Calendar
+//
+//  Created by Alexey Belkevich on 9/4/13.
+//  Copyright (c) 2013 okolodev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Year)
+
+@property (nonatomic, readonly) NSInteger year;
+
+- (NSDate *)dateYearStart;
+- (NSDate *)dateYearEnd;
+- (NSDate *)dateYearAgo;
+- (NSDate *)dateYearAhead;
+- (NSDate *)dateBySettingYear:(NSInteger)year;
+- (NSDate *)dateByAddingYear:(NSInteger)year;
+
+@end
