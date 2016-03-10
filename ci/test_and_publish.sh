4b1b8902273a3e231cf19568750e11ce05de4c3b
Nickolas Lanasa <nick@nytekproductions.com>
2016-03-09 22:05:47 -0500

    Updated travis file script

--- ci/test_and_publish.sh
+++ ci/test_and_publish.sh
@@ -3,7 +3,7 @@ set -e
 
 fastlane ios test
 
-if [[ $TRAVIS_PULL_REQUEST == "false" && ($TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "development") ]] ; then
+if [[ $TRAVIS_PULL_REQUEST == "false" && ($TRAVIS_BRANCH == "master" || $TRAVIS_BRANCH == "development" || $TRAVIS_BRANCH == "feature/travis") ]] ; then
     fastlane ios beta
 else
     echo "Skipping, only publishing beta builds for commits to development or master."
